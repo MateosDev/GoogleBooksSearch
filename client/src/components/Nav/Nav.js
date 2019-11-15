1b7daeb6fb6c7552d3f474010971205501f3d7d8
MateosDev <35548023+MateosDev@users.noreply.github.com>
2019-11-15 12:42:34 -0600

    changes to styling, database, mongod, etc

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,8 +1,14 @@
 import React from "react";
 
+const style = {
+  navbar: {
+    backgroundColor: "grey"
+  }
+}
+
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark" style={style.navbar}>
       <a className="navbar-brand" href="/">
         React Reading List
       </a>
